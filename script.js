34fe5be36928c102f304b090b01019016947f553
PsalmogunsGRT <105373542+PsalmogunsGRT@users.noreply.github.com>
2022-06-24 18:50:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const num1 = parseFloat(prompt('Enter number:'));
 const num2 = parseFloat(prompt('Enter another number:'));
 
 let operation = prompt("Please enter the operation you would like to perform: add +, subtract -, divide /, multiply * ");
+let result = 0
 if(isNaN(num1) || isNaN(num2)){
     alert('Wrong Input!')
 }else{

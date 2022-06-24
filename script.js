f148cd3c7278b4c6e78fe048639cf695fad3d3b0
PsalmogunsGRT <105373542+PsalmogunsGRT@users.noreply.github.com>
2022-06-24 18:33:13 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+alert("This is a calculator for you. This calculator can only perform operations between 2 numbers please.");
+
+const num1 = parseFloat(prompt('Enter number:'));
+const num2 = parseFloat(prompt('Enter another number:'));
+
+let operation = prompt("Please enter the operation you would like to perform: add +, subtract -, divide /, multiply * ");
+if(isNaN(num1) || isNaN(num2)){
+    alert('Wrong Input!')
+}else{
+    if(operator== '+'){
+        result = num1 + num2;
+    }else if(operator == '-'){
+        result = num1 - num2;
+    }else if(operator == '/'){
+        result = num1 / num2;
+    }else if(operator == '*'){
+        result = num1 * num2;
+    }
+    alert(num1+operator+num2+'=' + result);
+}
